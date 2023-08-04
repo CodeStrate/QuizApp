105cfd4c3e4065822c8ba2a67f086be9b7b194df
Bence Fekete <fekete965@gmail.com>
2023-08-04 14:28:54 +0100

    Update QuizScreen

--- src/components/QuizScreen.jsx
+++ src/components/QuizScreen.jsx
@@ -1,34 +1,41 @@
 // sample link :   https://opentdb.com/api.php?amount=10
 
-import { useEffect, useState } from "react";
 import { LoadingSubTitle, SubTitle } from "../styledComponents/SubTitle";
 import { Card } from "./Card";
 import { nanoid } from "nanoid";
 import { decode } from "he";
 import { Button, SubmitButton } from "../styledComponents/Button";
 import { Paragraph } from "../styledComponents/Paragraph";
-import fetchQuestions from "../services-hooks/fetchQuestions";
+import useQuizData, { RequestStatus } from "../hooks/useQuizData";
 
 export default function QuizScreen({ className, apiParams }) {
-  const [quizIsLoading, setQuizIsLoading] = useState(true);
-  const [quizData, setQuizData] = useState([]);
-  const [gameIsRunning, setGameIsRunning] = useState(false);
+  const {
+    error,
+    status,
+    gameIsRunning,
+    setGameIsRunning,
+    setQuizData,
+    quizData,
+  } = useQuizData(apiParams);
 
-  // useEffect(() => {
-  //         fetchQuestions(apiParams)
-
-  //         setQuizIsLoading(false)
-  //         setGameIsRunning(true)
-  // },[gameIsRunning])
-
-  return (
-    <main className={className}>
-      {quizIsLoading && (
+  if (status === RequestStatus.Pending || status === RequestStatus.Idle) {
+    return (
+      <main className={className}>
         <div className="loader">
           <div className="loading--Animation"></div>
           <LoadingSubTitle>Loading</LoadingSubTitle>
         </div>
-      )}
-    </main>
-  );
+      </main>
+    );
+  }
+
+  if (status === RequestStatus.Rejected && error) {
+    return (
+      <main className={className}>
+        <div>Something went wrong!</div>
+      </main>
+    );
+  }
+
+  return <main className={className}>{JSON.stringify(quizData)}</main>;
 }
