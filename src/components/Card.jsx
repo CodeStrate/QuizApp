7c4e082ef80cf496b92fa423380167efbe25aff5
Bence Fekete <fekete965@gmail.com>
2023-08-15 21:23:44 +0100

    Minor refactor in card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,35 +2,33 @@ import { CardQuestionParagraph } from "../styledComponents/Paragraph";
 import "./Card.css";
 import RadioButton from "./RadioButton";
 
-export const Card = ({question, options, questionId, selectOption, selectedOptionID, answer, gameRunningState}) => {
-  
-
-
-  let cardClass = gameRunningState ? "" : handleClassNames()
-
-  const handleClassNames = () => {
-    return selectedOptionID === answer.id ? "medium" : "hard"
-  }
-
-  const RadioOptions = options.map(o => (
-    <RadioButton
-    key={o.id}
-    id={o.id}
-    name={questionId}
-    value={o.value}
-    variant={`options ${cardClass}`}
-    onChange={() => selectOption(questionId, o.id)}
-    checked={selectedOptionID === o.id}
-    >
-    {o.value}
-    </RadioButton>
-    
-  ))
-
+export const Card = ({
+  question,
+  options,
+  questionId,
+  selectOption,
+  selectedOptionID,
+  answer,
+  gameRunningState,
+}) => {
   return (
     <div className="card">
       <CardQuestionParagraph>{question}</CardQuestionParagraph>
-      <div className="option-container">{RadioOptions}</div>
+      <div className="option-container">
+        {options.map((o) => (
+          <RadioButton
+            key={o.id}
+            id={o.id}
+            name={questionId}
+            value={o.value}
+            variant={`options`}
+            onChange={() => selectOption(questionId, o.id)}
+            checked={selectedOptionID === o.id}
+          >
+            {o.value}
+          </RadioButton>
+        ))}
+      </div>
     </div>
   );
-}
+};
