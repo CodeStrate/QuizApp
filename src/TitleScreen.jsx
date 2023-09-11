6de8e6ea481b874e5a2ec075f6eeafc880508903
Bence Fekete <fekete965@gmail.com>
2023-09-11 13:33:57 +0100

    Use new Layout component in TitleScreen

--- src/TitleScreen.jsx
+++ src/TitleScreen.jsx
@@ -1,62 +1,58 @@
 import "./App.css";
 
-//Components
 import { AiFillSetting, AiFillPlayCircle } from "react-icons/ai";
 import Preferences from "./components/Preferences";
 import useToggle from "./services-hooks/useToggle";
 import usePreferences from "./services-hooks/usePreferences";
 
-//styled Components
 import { Paragraph } from "./styledComponents/Paragraph";
 import { Title } from "./styledComponents/Title";
 import { Button } from "./styledComponents/Button";
+import { useQuizData } from "./context/quizDataContext/QuizDataContext";
 import QuizScreen from "./components/QuizScreen";
+import { GameStatus } from "./context/quizDataContext/reducer";
 
-function App() {
-  const { preferences, reset, update } = usePreferences({
-    amount: 5,
-    difficulty: "easy",
-    type: "multiple",
-    category: "9",
-  });
-  const [openPrefs, togglePrefs] = useToggle(false);
-  const [isPlaying, togglePlaying] = useToggle(false);
+const App = () => {
+  const { gameStatus, newGame } = useQuizData();
 
-  return (
-    <main className="title--screen">
-      <span onClick={togglePrefs} className="icon pref">
-        <AiFillSetting />
-      </span>
-      <span className="icon music">
-        <AiFillPlayCircle />
-      </span>
-      <Preferences
-        open={openPrefs}
-        preferences={preferences}
-        reset={reset}
-        update={update}
+  if (
+    gameStatus === GameStatus.Playing ||
+    gameStatus === GameStatus.Over ||
+    gameStatus === GameStatus.Loading
+  ) {
+    return (
+      <Layout
+        render={({ openPrefs, preferences }) => (
+          <QuizScreen
+            className={`container quiz ${openPrefs ? "open" : ""}`}
+            preferences={preferences}
+          />
+        )}
       />
+    );
+  }
 
-      {isPlaying ? (
-        <QuizScreen
-          className={`container quiz ${openPrefs && "open"}`}
-          apiParams={preferences}
-        />
-      ) : (
-        <div className={`container ${openPrefs && "open"}`}>
-          <Title>The Quiz Ultra Pro Max</Title>
-          <br />
-          <Paragraph>
-            This is probably the `Ultimate` and Best version of Quizzical you'll
-            experience!
-          </Paragraph>
-          <br />
-          <Button onClick={togglePlaying}>Begin Game</Button>
-        </div>
-      )}
-    </main>
-  );
-}
+  if (gameStatus === GameStatus.Idle) {
+    return (
+      <Layout
+        render={({ openPrefs, preferences }) => (
+          <div className={`container ${openPrefs ? "open" : ""}`}>
+            <Title>The Quiz Ultra Pro Max</Title>
+            <br />
+            <Paragraph>
+              This is probably the `Ultimate` and Best version of Quizzical
+              you&apos;ll experience!
+            </Paragraph>
+            <br />
+            <Button onClick={() => newGame(preferences)}>Begin Game</Button>
+          </div>
+        )}
+      />
+    );
+  }
+
+  throw new Error("Unsuppoerted gameStatus: " + gameStatus);
+};
 
 const Layout = ({ render }) => {
   const { preferences, reset, update } = usePreferences({
