4111fb78d5f34b670e3e5b925a99237ce6855ffb
Bence Fekete <fekete965@gmail.com>
2023-09-11 13:30:01 +0100

    Wrap application into QuizDataProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./TitleScreen.jsx";
-// import Quiz from './components/QuizScreen.jsx'
+import QuizDataProvider from "./context/quizDataContext/QuizDataContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <QuizDataProvider>
+      <App />
+    </QuizDataProvider>
+  </React.StrictMode>
 );
